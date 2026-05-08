4ab26c90adf9cc974788ce7316c02858223db4bf
Maharshi <maharshilavana@gmail.com>
2026-05-08 23:50:19 +0530

    Didnt did good today

--- app/ui/components/Services.tsx
+++ app/ui/components/Services.tsx
@@ -1,9 +1,26 @@
 'use client'
+import AnimatedIphone from "../util-components/AnimatedIphone"
+import  {inter } from "@/app/layout"
 
 export default function Services(){
     return (
-        <div className = 'w-full h-170 dotted-frame-bottom flex flex-col items-center justify-start gap-2 py-6 h-300 dotted-frame-bottom '>
+        <div className = 'w-full  dotted-frame-bottom flex flex-col items-center justify-start gap-14 py-6 h-220 dotted-frame-bottom '>
+           <div>
+             <h1 className = {`${inter.className} text-xl font-semibold`}> We take care of things from <span className = 'bg-blue-700 text-white'>start to finish</span></h1>
+           </div>
+           <div className = 'grid grid-cols-2 grid-rows-2  gap-10 w-full h-80 max-w-4xl'>
+               <div className = 'flex items-center justify-center  bg-white/80 px-4 py-4 w-full h-100 diamond-grid bg-[repeating-linear-gradient(315deg,#ffffff_0px,#d4d4d4_1px,transparent_0px,transparent_50%),repeating-linear-gradient(-315deg,#d4d4d4_0px,#d4d4d4_1px,transparent_0px,transparent_50%)] bg-[size:4px_4px]  border border-neutral-200-'>
+                  <div className = 'h-70 w-full max-w-xl shadow-xl rounded-lg flex items-center justify-center bg-black border border-white border-4 '>
 
+                  </div>
+               </div>
+               <div className = 'flex items-center justify-center bg-white px-4 py-4 w-full h-100 diamond-grid bg-[repeating-linear-gradient(315deg,#ffffff_0px,#d4d4d4_1px,transparent_0px,transparent_50%),repeating-linear-gradient(-315deg,#d4d4d4_0px,#d4d4d4_1px,transparent_0px,transparent_50%)] bg-[size:4px_4px] border border-neutral-200-'>
+
+               </div>
+               <div className = 'flex items-center justify-center col-span-2 mt-60 bg-white/80 px-4 py-4 w-full h-80 border border-neutral-200-'>
+
+               </div>
+           </div>
         </div>
     )
 }
---
+++ app/ui/util-components/AnimatedIphone.tsx
@@ -0,0 +1,87 @@
+'use client'
+import { animate } from 'motion'
+import {motion} from 'motion/react'
+import { inter } from '@/app/layout'
+import waves from '@/app/assets/images/waves.png'
+import  uifaces1 from '@/app/assets/images/uifaces-1.jpg';
+import  uifaces3 from '@/app/assets/images/uifaces-3.jpg';
+
+export default function AnimatedIphone() {
+    return(
+        <motion.div whileHover = "animate" initial = "initial" className = 'p-4 rounded-4xl relative'>
+            <div className = 'absolute left-3.5 top-18 w-1 h-3.5  rounded-tl-sm rounded-bl-sm bg-neutral-400 z-2'></div>
+           <div className = 'absolute left-3.5 top-24 w-1 h-4.5  rounded-tl-sm rounded-bl-sm bg-neutral-400 z-2'></div>
+            <div className = 'absolute left-3.5 top-31 w-1 h-4.5 rounded-tl-sm rounded-bl-sm bg-neutral-400 z-2'></div>
+            <div className = 'absolute right-3.5 top-27 w-1 h-8 rounded-tr-sm rounded-br-sm bg-neutral-400 z-2'></div>
+        <div className = 'flex items-center justify-center h-62 w-38 bg-transparent rounded-4xl border border-2 border-neutral-300  mx-auto px-2 py-2'>
+           <div className = 'relative bg-transparent h-58 w-38 rounded-3xl'>
+             <div className = 'absolute bg-transparent w-30 h-10  ml-5 mt-1 flex  items-center justify-between '>
+                 <div className = 'bg-black w-12 ml-6 mb-5 h-3 rounded-2xl border relative'>
+                    <div className = 'absolute left-8 top-0 bottom-0 bg-neutral-700  border border-neutral-800 w-2 h-2 rounded-full mx-auto my-auto'></div>
+                 </div>
+            </div>
+            <div className = 'absolute top-8 left-1 flex flex-col items-center justify-center z-5 w-full h-15 '>
+                <h1 className = {`absolute text-white text-sm ${inter.className}  group-hover top-1`}>Sat 13 Nov</h1>
+                <h1 className = {`absolute text-white text-2xl ${inter.className} font-bold top-6`}>11:11 AM</h1>
+            </div>
+            <motion.div 
+            variants = {{
+            initial : {
+                y:0,
+                scaleX : 1,
+            },
+             animate : {
+                y:-60,
+                z:20,
+               scaleX : 1.35,
+            }
+            }}
+            transition = {{
+                duration : 0.3,
+                type : 'spring',
+                stiffness : 100,
+                damping : 13,   
+            }}
+            className = 'absolute bottom-5 w-32 left-0.5  h-8 rounded-lg border border-white bg-gradient-to-r from-transparent from-5% via-cyan-600/20 via-20% via-cyan-400/10 via-59% to-transparent to-99% flex items-center justify-start gap-1 px-1 backdrop-blur-2xl shadow-[0_0_20px_rgba(165,243,252,0.35)]'>
+                    <motion.div className = 'w-6 h-6 rounded-full'>
+                      <img  src = {uifaces1.src} className = 'w-full h-full object-cover rounded-full' />
+                    </motion.div>
+                    <motion.div className = 'flex flex-col items-start justify-center h-6 w-25'>
+                      <motion.p className = {`${inter.className} text-[9px] text-white font-bold`}>Maharshi</motion.p>
+                      <motion.p className = {`${inter.className} text-[9px] text-white font-semibold`}>Pushed to prod</motion.p>
+                    </motion.div>
+            </motion.div>
+            <motion.div 
+            variants = {{
+            initial : {
+                y:0,
+                scaleX : 1,
+            },
+             animate : {
+                y:-45,
+                z:20,
+               scaleX : 1.35,
+            }
+            }}
+            transition = {{
+                duration : 0.3,
+                type : 'spring',
+                stiffness : 100,
+                damping : 13,   
+            }}
+            className = 'absolute bottom-0 w-32 left-0.5  h-8 rounded-lg border border-white bg-gradient-to-r from-transparent from-5% via-cyan-600/20 via-20% via-cyan-400/10 via-59% to-transparent to-99% flex items-center justify-start gap-1 px-1 backdrop-blur-2xl shadow-[0_0_20px_rgba(165,243,252,0.35)]'>
+                    <motion.div className = 'w-6 h-6 rounded-full'>
+                      <img  src = {uifaces3.src} className = 'w-full h-full object-cover rounded-full' />
+                    </motion.div>
+                    <motion.div className = 'flex flex-col items-start justify-center h-6 w-25'>
+                      <motion.p className = {`${inter.className} text-[9px] text-white font-bold`}>Alex</motion.p>
+                      <motion.p className = {`${inter.className} text-[9px] text-white font-semibold`}>Deployed!!</motion.p>
+                    </motion.div>
+            </motion.div>
+             <img src={waves.src} className = 'w-full h-full object-cover rounded-3xl' />
+           </div>
+        </div>
+        </motion.div>
+    )
+}
+
